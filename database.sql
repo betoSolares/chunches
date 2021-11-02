568bf5438305fd5fa8f9af15afe48895f4018445
Roberto Solares <betosolaresgar@gmail.com>
2021-11-01 18:05:31 -0600

    Create db tables

--- database.sql
+++ database.sql
@@ -8,3 +8,21 @@ CREATE TABLE IF NOT EXISTS users (
   PRIMARY KEY (user_id),
   CONSTRAINT no_duplicate_user UNIQUE (user_id, email)
 );
+
+-- Create table to keep a record of the raitings
+CREATE TABLE IF NOT EXISTS raitings_log (
+  raitings_log_id SERIAL,
+  total int NOT NULL,
+  PRIMARY KEY (raitings_log_id)
+);
+
+-- Table for the raitings of each user
+CREATE TABLE IF NOT EXISTS raitings_users (
+  raitings_user_id SERIAL,
+  user_id int NOT NULL,
+  product_link varchar(1000) NOT NULL,
+  raiting int NOT NULL,
+  PRIMARY KEY (raitings_user_id),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (user_id),
+  CONSTRAINT no_duplicate_raiting UNIQUE (user_id, product_link)
+);
